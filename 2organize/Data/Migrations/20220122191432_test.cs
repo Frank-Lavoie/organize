2f3641b6ec2f301a0a54b30f19a8c0d2907251a4
Francis Lavoie <francis.lavoie79@outlook.com>
2022-01-22 14:31:07 -0500

    initial commit

---
+++ 2organize/Data/Migrations/20220122191432_test.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _2organize.Data.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HomeTasks",
+                columns: table => new
+                {
+                    HomeTaskId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HomeTasks", x => x.HomeTaskId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HomeTasks");
+        }
+    }
+}
---
+++ 2organize/Models/HomeTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2organize.Models
+{
+    public class HomeTask
+    {
+        public int HomeTaskId { get; set; }
+        public string Name { get; set; }
+    }
+}
